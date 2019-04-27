88d829ee897708cc67a86d2744118fa89375942c
Matthias Kunnen <matthias.kunnen@gmail.com>
2019-04-27 05:18:54 +0200

    Fixed error on publish without draft, i.a.
    The following issues were tackled, see the test for more details:
    
    Publisher
        Update draft
          1) should update drafts but not publish when a published version
    exists
        Create published
          2) should create published templates without existing drafts
          3) should create published templates with existing drafts
        Update published
          4) should update published templates

--- src/publisher.ts
+++ src/publisher.ts
@@ -16,6 +16,27 @@ export class SparkPostPublisher {
         this.sparkPost = new SparkPost(apiKey);
     }
 
+    /**
+     * Creates or updates a template.
+     *
+     * Due to some quirks on SparkPost's side we have to do some acrobatics in order to correctly
+     * create or update a template since there are different states in which a template can find
+     * itself in.
+     *
+     * States:
+     * - Non existent
+     * - Only draft exists
+     * - Published version exists
+     *
+     * The most important gotcha is that there is no single way to publish a template which only has
+     * a draft as well as update an existing published template. The following occurs:
+     *
+     * - adding _published: true_ does not update the existing published template
+     * - adding _update_published: true_ errors if no published version exists
+     *
+     * Unfortunately we have to rely on expecting an error using _update_published_ and follow up
+     * with the other approach.
+     */
     async createOrUpdate(template: CreateOrUpdateTemplate, publish = false) {
         try {
             await this.sparkPost.templates.get(template.id);
@@ -24,12 +45,33 @@ export class SparkPostPublisher {
                 throw e;
             }
 
+            // No draft version of this template ID exists, create it
             await this.sparkPost.templates.create(template);
-            return;
+
+            if (!publish) {
+                return;
+            }
         }
 
-        await this.sparkPost.templates.update(template.id, template, {
-            update_published: publish,
-        });
+        const submitTemplate = {
+            ...template,
+            published: publish,
+        };
+        delete submitTemplate.id; // ID is not allowed on update
+
+        try {
+            // Update drafts when publish is false or update existing published templates
+            await this.sparkPost.templates.update(template.id, submitTemplate, {
+                update_published: publish,
+            });
+        } catch (e) {
+            if (e.statusCode !== 404) {
+                throw e;
+            }
+
+            // When only a draft version exists, update_published causes an error. Retry without
+            // update_published
+            await this.sparkPost.templates.update(template.id, submitTemplate);
+        }
     }
 }
